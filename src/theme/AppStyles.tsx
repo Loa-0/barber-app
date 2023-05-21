47ddc60931ce73710a1580ab9d334ab6b12cc22d
IsaacVzq <isaac.vazquez@designamx.com>
2023-05-21 17:47:25 -0600

    feat: #2 Navigor

--- src/theme/AppStyles.tsx
+++ src/theme/AppStyles.tsx
@@ -1,4 +1,10 @@
 import {StyleSheet} from 'react-native';
+export const globalColors = {
+  mainBack: '#000000',
+  mainText: '#ffffff',
+  golden: '#DAA520',
+  iconSize: 20,
+};
 export const styles = StyleSheet.create({
   globalMargin: {
     marginHorizontal: 20,
