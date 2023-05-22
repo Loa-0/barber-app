1507277b82b2aaf7649be8411d82c5eb2dd01424
UP200424 <113137617+UP200424@users.noreply.github.com>
2023-05-21 20:53:07 -0600

    3 settings view (#7)
    * feat: #3 settings view
    
    * fix: #3 themebox props
    
    * feat: #3 settings view
    
    * fix: #3 error styles fixed

--- src/components/settings/settings.tsx
+++ src/components/settings/settings.tsx
@@ -1,1 +1,63 @@
-//settings main
+import React, {useState} from 'react';
+import {Text, View, SafeAreaView, TouchableOpacity} from 'react-native';
+import {styles as S} from './settingsStyles';
+import {ScrollView} from 'react-native-gesture-handler';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import {globalColors} from '../../theme/AppStyles';
+
+const Separator = () => <View style={S.separator} />;
+
+export const SettingsDisplay = () => {
+  const [theme, setTheme] = useState('dark');
+
+  const handleThemeChange = (selectedTheme: string) => {
+    setTheme(selectedTheme);
+  };
+  return (
+    <View style={S.container}>
+      <SafeAreaView style={S.safeArea}>
+        <ScrollView contentContainerStyle={S.contentContainer}>
+          <Text style={S.subtitleText}>Preferencias de tema</Text>
+          <Separator />
+          <View style={S.themeContainer}>
+            <TouchableOpacity
+              style={[
+                S.themeOption,
+                theme === 'light' && S.selectedThemeOption,
+              ]}
+              onPress={() => handleThemeChange('light')}>
+              <Text style={S.themeText}>
+                <FontAwesome5
+                  name="cloud-sun"
+                  size={globalColors.iconSize}
+                  color={
+                    theme === 'light'
+                      ? globalColors.golden
+                      : globalColors.mainText
+                  }
+                />{' '}
+                Modo claro
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={[S.themeOption, theme === 'dark' && S.selectedThemeOption]}
+              onPress={() => handleThemeChange('dark')}>
+              <Text style={S.themeText}>
+                <FontAwesome5
+                  name="cloud-moon"
+                  size={globalColors.iconSize}
+                  color={
+                    theme === 'dark'
+                      ? globalColors.golden
+                      : globalColors.mainText
+                  }
+                />{' '}
+                Modo oscuro
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </View>
+  );
+};
