220635879fa2cd1b7549fefce0a3a7a3f8090536
IsaacVzq <isaac.vazquez@designamx.com>
2023-05-22 09:43:28 -0600

    #9 Added Logic for useReducer

--- src/screens/Settings.tsx
+++ src/screens/Settings.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {View} from 'react-native';
 import {SettingsDisplay} from '../components/settings/settings';
+import {AuthContext} from '../context/AuthContext';
+import {Text} from 'react-native-paper';
 
 export const Settings = () => {
+  const {authState} = useContext(AuthContext);
   return (
     <View>
       <SettingsDisplay />
+      <Text>{JSON.stringify(authState)}</Text>
     </View>
   );
 };
