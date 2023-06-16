c0fde9e5f999b50d7487e63a7f1510142b2725f3
UP210010 <up210010@alumnos.upa.edu.mx>
2023-06-15 18:00:13 -0600

    puma changes

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -1,27 +1,92 @@
 import {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
-import {
-  StyleSheet,
-  // ImageBackground,
-  Text,
-  View,
-} from 'react-native';
+import {StyleSheet, ImageBackground, Text, View, Linking} from 'react-native';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import {globalColors} from '../theme/AppStyles';
 
-// const backgroundImage = require('../assets/main.jpg');
+const backgroundImage = require('../assets/fondo.jpg');
 
 interface Props extends StackScreenProps<any, any> {}
 
 export const Dashboard = ({}: Props) => {
+  const handleWebsitePress = () => {
+    Linking.openURL('https://ulisesbarber.negocio.site');
+  };
+
+  const handleFacebookPress = () => {
+    Linking.openURL('https://es-la.facebook.com/UBPeluqueriaYBarberia/');
+  };
+
+  const handleInstagramPress = () => {
+    Linking.openURL(
+      'https://www.instagram.com/ulisesvelru/?igshid=MzRlODBiNWFlZA%3D%3D',
+    );
+  };
+
   return (
     <View style={styles.backgroundColor}>
-      {/* <ImageBackground source={backgroundImage} style={styles.backgroundImage}> */}
-      <Text>Ulises Barber</Text>
-      <Text>Encuentranos en nuestras redes sociales</Text>
-      <Text>Fb:Ulises Barber Peluqueria & Barberia</Text>
-      <Text>Ig: Barbervelru</Text>
-      <Text>Citas al: 4492091414</Text>
-      <Text>Pagina web: ulisesbarber.negocio.site</Text>
-      {/* </ImageBackground> */}
+      <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
+        <View style={styles.textContainer}>
+          <Text style={styles.text}>
+            <FontAwesome5
+              name="cut"
+              color={'#333333'}
+              size={globalColors.iconSize}
+            />
+            Ulises Barber
+            <FontAwesome5
+              name="cut"
+              color={'#333333'}
+              size={globalColors.iconSize}
+            />
+          </Text>
+          <Text style={styles.text}>
+            Encuéntranos en nuestras redes sociales
+          </Text>
+          <Text style={styles.text}>
+            <FontAwesome5
+              onPress={handleFacebookPress}
+              accessibilityRole="link"
+              href="https://es-la.facebook.com/UBPeluqueriaYBarberia/"
+              name="facebook"
+              color={'#0000FF'}
+              size={globalColors.iconSize}
+            />
+            Ulises Barber Peluqueria & Barberia
+          </Text>
+          <Text style={styles.text}>
+            <FontAwesome5
+              onPress={handleInstagramPress}
+              accessibilityRole="link"
+              href="https://www.instagram.com/ulisesvelru/?igshid=MzRlODBiNWFlZA%3D%3D"
+              name="instagram"
+              color={'#333333'}
+              size={globalColors.iconSize}
+            />
+            ulisesvelru
+          </Text>
+          <Text style={styles.text}>
+            <FontAwesome5
+              name="phone-volume"
+              color={'#1C754C'}
+              size={globalColors.iconSize}
+            />
+            Citas al: 4492091414
+          </Text>
+          <Text
+            style={styles.text}
+            onPress={handleWebsitePress}
+            accessibilityRole="link"
+            href="https://ulisesbarber.negocio.site">
+            <FontAwesome5
+              name="internet-explorer"
+              color={'#ADD8E6'}
+              size={globalColors.iconSize}
+            />
+            ulisesbarber.negocio.site
+          </Text>
+        </View>
+      </ImageBackground>
     </View>
   );
 };
@@ -43,5 +108,17 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  textContainer: {
+    backgroundColor: 'rgba(255, 255, 255, 0.45)', // Rectángulo transparente (30% de opacidad)
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 10,
+  },
+  text: {
+    color: '#333333',
+    marginTop: 19,
+    fontSize: 20,
+    fontWeight: 'bold',
   },
 });
