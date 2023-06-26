89f8cec1f3f72a1fdb908a8bf57efcdf87c8418e
IsaacVzq <111448356+IsaacVzq@users.noreply.github.com>
2023-06-25 18:52:37 -0600

    fix: #20 added Contextfor Service (#25)
    * feat: added Header
    
    * fix: #20 added Contextfor Service
    
    ---------
    
    Co-authored-by: IsaacVzq <>

--- src/context/Service.Context.tsx
+++ src/context/Service.Context.tsx
@@ -1,33 +1,76 @@
-import React, {createContext} from 'react';
+import React, {createContext, useState} from 'react';
 
 type Service = {
+  id: string;
+  title: string;
+  image: {
+    uri: any;
+  };
+  duration: number;
+  price: number;
+};
+type SelectedService = {
+  services: Service[];
+  totalCost: number;
+  totalDuration: number; // .5- 1 -1.5
+  start: Date;
+  end: Date;
   nameEvent: string;
-  start: string;
-  end: string;
   description: string;
   clientName: string;
   email: string;
-  duration: number;
 };
 
 type AgendaContextProps = {
-  services: Service;
+  servicesFinal: SelectedService;
+  // setSelectedServices: (service: Service) => void;
+  updateTotalCost: (servicesParam: Service[]) => void;
 };
 
 export const ServiceContext = createContext({} as AgendaContextProps);
-
 export const ServiceProvider = ({children}: any) => {
-  const services: Service = {
-    nameEvent: 'Corte Evento',
-    start: '00:00',
-    end: '00:00',
-    description: '00:00',
-    clientName: 'Isaac',
-    email: 'vazisaac9508@gmail.com',
-    duration: 1.5,
+  const [servicesFinal, setServices] = useState<SelectedService>({
+    services: [],
+    totalCost: 0,
+    totalDuration: 0,
+    start: new Date(),
+    end: new Date(),
+    nameEvent: '',
+    description: '',
+    clientName: '',
+    email: '',
+  });
+  // useEffect(() => {
+  //   updateTotalCost(servicesSelected);
+  // }, [servicesSelected]);
+
+  const updateTotalCost = (servicesParam: Service[]) => {
+    if (servicesParam.length > 0) {
+      let totalCost: number = 0;
+      let totalDuration: number = 0;
+      servicesParam.map(serv => {
+        totalDuration += serv.duration;
+        totalCost += serv.price;
+      });
+      setServices({
+        services: servicesParam,
+        totalCost,
+        totalDuration,
+        start: new Date(),
+        end: new Date(),
+        nameEvent: 'Servcio para',
+        description: 'Pending... ',
+        clientName: 'Isaac',
+        email: 'vazisaac9508@gmail.com',
+      });
+    }
   };
+  // const setSelectedServices = (service: Service) => {
+  //   setSelectedServices1(prev => [...prev, service]);
+  // };
+
   return (
-    <ServiceContext.Provider value={{services}}>
+    <ServiceContext.Provider value={{servicesFinal, updateTotalCost}}>
       {children}
     </ServiceContext.Provider>
   );
